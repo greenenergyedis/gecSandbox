9dc90f05f8ff3747e528cd20f1178a4d62927137
greenenergyedis <edisdev@greenenergycorp.com>
2012-01-18 14:53:04 -0700

    another branch line.

--- TestGitProject/src/com/test/item/TestItem.java
+++ TestGitProject/src/com/test/item/TestItem.java
@@ -16,5 +16,6 @@ public class TestItem
         System.out.println("test8");
         System.out.println("test9");
         System.out.println("branch1-1");
+        System.out.println("branch1-2");
     }
 }
