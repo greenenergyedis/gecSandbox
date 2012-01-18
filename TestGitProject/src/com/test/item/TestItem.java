1b34039afd4dfee975b1dec6f62d39eedfeb3340
greenenergyedis <edisdev@greenenergycorp.com>
2012-01-18 09:11:17 -0700

    another change.

--- TestGitProject/src/com/test/item/TestItem.java
+++ TestGitProject/src/com/test/item/TestItem.java
@@ -9,5 +9,6 @@ public class TestItem
         System.out.println("test3");
         System.out.println("test2");
         System.out.println("test4");
+        System.out.println("test6");
     }
 }
